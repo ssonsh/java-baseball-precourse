d028c5a47a749e232c3ae30b30b86f0672febd05
ssonsh <chosunci@gmail.com>
2022-04-14 22:14:06 +0900

    refactoring(Participant): 의미없는 Participant 부모클래스 제거

--- src/main/java/baseball/domain/participant/Computer.java
+++ src/main/java/baseball/domain/participant/Computer.java
@@ -4,13 +4,21 @@ import baseball.domain.BaseballNumbers;
 import baseball.generator.NumberGenerator;
 import java.util.List;
 
-public class Computer extends Participant{
+public class Computer{
+
+    private final BaseballNumbers baseballNumbers;
+
     private Computer(BaseballNumbers baseballNumbers) {
-        super(baseballNumbers);
+        this.baseballNumbers = baseballNumbers;
     }
 
     public static Computer generateBy(NumberGenerator numberGenerator) {
         List<Integer> generateNumbers = numberGenerator.generate();
         return new Computer(BaseballNumbers.of(generateNumbers));
+    }
+
+
+    public BaseballNumbers getBaseballNumbers() {
+        return this.baseballNumbers;
     }
 }
